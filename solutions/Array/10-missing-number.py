d1d140464de7381b70539a6fd5f9aaaa46b57df2
PHANINDRA-prog <phanindrapvs5@gmail.com>
2026-05-24 09:08:34 +0530

    Add solution: Missing Number

--- solutions/Array/10-missing-number.py
+++ solutions/Array/10-missing-number.py
@@ -1,9 +1,21 @@
 // Time Complexity O(n)
 // Space Complexity O(1)
+# This one is using the division and multiplication formula and below one xor both have same time and space complexity
+# class Solution:
+#     def missingNumber(self, nums: List[int]) -> int:
+#         n = len(nums)
+#         total_sum = (n*(n+1))//2
+#         actual_sum = sum(nums)
+#         return total_sum - actual_sum
+
+
+# This one is using xor operator
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
-        total_sum = (n*(n+1))//2
-        actual_sum = sum(nums)
-        return total_sum - actual_sum
-
+        xor_sum = 0
+        for i in range(1,n+1):
+            xor_sum ^= i
+        for num in nums:
+            xor_sum ^= num
+        return xor_sum
