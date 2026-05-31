3de38db342284395f62b6ae0e2caa5710dff628c
PHANINDRA-prog <phanindrapvs5@gmail.com>
2026-05-31 22:09:58 +0530

    Add solution: Majority Element II

--- solutions/Array/15-majority-element-ii.py
+++ solutions/Array/15-majority-element-ii.py
@@ -1,14 +1,39 @@
-//TC : O(nlogk)
-//SC : O(1)
-
+// TC : O(n)
+// SC : O(1)
 from collections import Counter
 import heapq
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        n = len(nums)
-        counts = Counter(nums)
+        cand_one = None
+        cand_two = None
+        cand_one_count = 0
+        cand_two_count = 0
 
-        top_two = heapq.nlargest(2, (item for item in counts.items() if item[1] > n/3), key=lambda item: item[1])
-        result = [item[0] for item in top_two]
+        for i in range(len(nums)):
+            if nums[i]== cand_one:
+                cand_one_count += 1
+            elif nums[i]== cand_two:
+                cand_two_count += 1
+            elif cand_one_count == 0:
+                cand_one = nums[i]
+                cand_one_count = 1
+            elif cand_two_count == 0:
+                cand_two = nums[i]
+                cand_two_count = 1
+            else:
+                cand_one_count -=1 
+                cand_two_count -=1
+        count_1 = 0
+        count_2 = 0
+        for num in nums:
+            if num == cand_one:
+                count_1 += 1
+            elif num == cand_two:
+                count_2 += 1
+        result = []
+        if count_1 > len(nums)//3:
+            result.append(cand_one)
+        if count_2 > len(nums)//3:
+            result.append(cand_two)
         return result
-        
+            
