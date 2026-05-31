ef4eebbf0aa2e437381b5b785e5de3cfaa8b55b2
PHANINDRA-prog <phanindrapvs5@gmail.com>
2026-05-31 14:50:17 +0530

    Add solution: Top K Frequent Elements

--- solutions/Array/11-top-k-frequent-elements.py
+++ solutions/Array/11-top-k-frequent-elements.py
@@ -1,12 +1,43 @@
+// TC : O(n)
+// SC : O(n)
 from collections import Counter
-import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # Brute Force Approach n^2
+        # frequency_count = Counter(nums)
+        # result = []
 
-        count = Counter(nums)
+        # for _ in range(k):
+        #     max_elem = None
+        #     max_count = -1
 
-        top_k = heapq.nlargest(k,count.items(),key = lambda item:item[1])
+        #     for key,value in frequency_count.items():
+        #         if value > max_count:
+        #             max_count = value
+        #             max_elem = key
+        #     result.append(max_elem)
+        #     del frequency_count[max_elem]
+        # return result
 
-        result = [item[0] for item in top_k]
 
-        return result
+        # n log n approach
+        # frequency_count = Counter(nums)
+
+        # sorted_keys = sorted(frequency_count, key=frequency_count.get, reverse=True)
+        # return sorted_keys[:k]
+
+
+        # Optimized Approach
+        frequency_count = Counter(nums)
+        n = len(nums)
+        buckets = [[] for _ in range(n+1)]
+
+        for key,value in frequency_count.items():
+            buckets[value].append(key)
+        
+        result = []
+        for i in range(n,-1,-1):
+            result.extend(buckets[i])
+            if len(result) == k:
+                return result
+
