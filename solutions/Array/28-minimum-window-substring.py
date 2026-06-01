69c16da01cd5c8b417bc092572e28b9e41de2dda
PHANINDRA-prog <phanindrapvs5@gmail.com>
2026-06-01 08:35:26 +0530

    Add solution: Minimum Window Substring

--- solutions/Array/28-minimum-window-substring.py
+++ solutions/Array/28-minimum-window-substring.py
@@ -1,46 +1,32 @@
-//TC : O(n)
-//SC : O(n)
-
+// TC : O(n)
+// SC : O(n)
 from collections import Counter
 
 class Solution:
-    def minWindow(self, s: str, t: str) -> str:
+    def minWindow(self, s, t):
 
-        t_count = Counter(t)
-        window_count = {}
+        target = Counter(t)
+        window = {}
 
-        have = 0
-        need = len(t_count)
-
+        required = len(target)
+        formed = 0
         left = 0
         answer = ""
+        min_length = float("inf")
 
         for right in range(len(s)):
+            window[s[right]] = window.get(s[right],0) + 1
 
-            char = s[right]
+            if (s[right] in target and window[s[right]] == target[s[right]]):
+                formed += 1
+            
+            while formed == required:
+                if (right - left + 1) < min_length:
+                    min_length = right - left + 1
+                    answer = s[left:right+1]
+                window[s[left]] -= 1
 
-            window_count[char] = window_count.get(char, 0) + 1
-
-            # Requirement satisfied
-            if char in t_count and window_count[char] == t_count[char]:
-                have += 1
-
-            # Window valid
-            while have == need:
-
-                current_window = s[left:right+1]
-
-                if answer == "" or len(current_window) < len(answer):
-                    answer = current_window
-
-                # Remove left character
-                left_char = s[left]
-                window_count[left_char] -= 1
-
-                # Requirement broken
-                if left_char in t_count and window_count[left_char] < t_count[left_char]:
-                    have -= 1
-
+                if (s[left] in target and window[s[left]] < target[s[left]]):
+                    formed -= 1
                 left += 1
-
         return answer
