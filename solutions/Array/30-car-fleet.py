087678a680ee838a0f7fb33acb1fc6267f64dab6
PHANINDRA-prog <phanindrapvs5@gmail.com>
2026-06-01 12:45:29 +0530

    Add solution: Car Fleet

---
+++ solutions/Array/30-car-fleet.py
@@ -0,0 +1,18 @@
+// TC : O(nlogn)
+// SC : O(n)
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = []
+        for i in range(len(position)):
+            time = (target - position[i])/speed[i]
+            cars.append([position[i],time])
+        
+        cars.sort(reverse = True)
+        fleet = 0
+        slowest = 0
+        for pos,time in cars:
+            if time > slowest:
+                fleet += 1
+                slowest = time
+        return fleet
+
