00ad16c108ccaec6f6736b306c2d0fbb40a529f5
PHANINDRA-prog <phanindrapvs5@gmail.com>
2026-06-07 17:07:06 +0530

    Add solution: Longest Common Prefix

--- solutions/Array/17-longest-common-prefix.py
+++ solutions/Array/17-longest-common-prefix.py
@@ -1,39 +1,18 @@
-// TC : O(n*m)
-// SC : O(1)
-# Brute Force Approach
-
-# class Solution:
-#     def longestCommonPrefix(self, strs: List[str]) -> str:
-#         first_word = strs[0]
-#         answer = ""
-#         for index,char in enumerate(first_word):
-#             count = 0
-#             valid = False
-#             for word in strs:
-#                 if index < len(word) and word[index] == char:
-#                     count += 1
-#                     valid = True
-#                 else:
-#                     valid = False
-#                     break
-#             if not valid:
-#                 break
-#             if count == len(strs):
-#                 answer += char
-#         return(answer)
-
+// TC : O(n)
+// SC : O(n)
 class Solution:
-    def longestCommonPrefix(self, strs):
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        # Optimzed Solution
+        strs.sort() # We would sort them in dictonary order
 
-        prefix_length = 0
-
-        for index, char in enumerate(strs[0]):
-
-            for word in strs:
-
-                if index >= len(word) or word[index] != char:
-                    return strs[0][:prefix_length]
-
-            prefix_length += 1
-
-        return strs[0][:prefix_length]
+        i = 0
+        first_word = strs[0]
+        last_word = strs[-1]
+        final_string = ""
+        while i < len(first_word) and i < len(last_word):
+            if first_word[i] == last_word[i]:
+                final_string += first_word[i]
+                i += 1
+            else:
+                break
+        return final_string
