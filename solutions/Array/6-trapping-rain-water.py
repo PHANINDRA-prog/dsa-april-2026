6566d9b4c480bcf4e5e47c72b41886df779cbef3
PHANINDRA-prog <phanindrapvs5@gmail.com>
2026-06-07 21:09:24 +0530

    Add solution: Trapping Rain Water

--- solutions/Array/6-trapping-rain-water.py
+++ solutions/Array/6-trapping-rain-water.py
@@ -1,47 +1,40 @@
-# Brute Force approach 
-# Time Complexity for brute force is O(n2) and optimized O(n)
-# Space Complexity for brute force O(n) and optimized O(n)
-
-# class Solution:
-
-#     def trap(self, height: List[int]) -> int:
-
-#         n = len(height)
-
-#         answer = 0
-
-#         for i in range(n):
-
-#             left_max = max(height[0:i+1])
-
-#             right_max = max(height[i:n])
-
-#             answer += min(left_max,right_max) - height[i]
-
-#         return answer
-
-
-
-
-# Optimized Approach
-
+// TC : O(n)
+// SC : O(n)
 class Solution:
     def trap(self, height: List[int]) -> int:
-        if not height: return 0
+        # Prefix and Suffix Approach
+        # n = len(height)
+        # left_max = [0]*n
+        # right_max = [0]*n
+
+        # left_max[0] = height[0]
+        # for i in range(1,len(height)):
+        #     left_max[i] = max(left_max[i-1],height[i])
+        
+        # right_max[n-1] = height[n-1]
+        # for i in range(n-2,-1,-1):
+        #     right_max[i] = max(right_max[i+1],height[i])
+        
+        # answer = 0
+        # for i in range(n):
+        #     answer += min(left_max[i],right_max[i]) - height[i]
+        # return answer
+
+        # Now Greedy Approach
         n = len(height)
-        left_max = [0] * n
-        right_max = [0] * n
+        left = 0
+        right = n - 1
+        left_max = height[left]
+        right_max = height[right]
         answer = 0
-
-        left_max[0] = height[0]
-        for i in range(1, n):
-            left_max[i] = max(left_max[i-1], height[i])
-
-        right_max[n-1] = height[n-1]
-        for i in range(n-2, -1, -1):
-            right_max[i] = max(right_max[i+1], height[i])
-
-        for i in range(n):
-            answer += min(left_max[i], right_max[i]) - height[i]
-
+        while left<right:
+            if left_max < right_max:
+                left += 1
+                left_max = max(left_max,height[left])
+                answer += left_max - height[left]
+            
+            else:
+                right -= 1
+                right_max = max(right_max,height[right])
+                answer += right_max - height[right]
         return answer
